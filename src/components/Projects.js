95eb565462b4c61de6646ff7a5d8736ed35e66f4
Tracy Njoroge <tracywangari997@gmail.com>
2025-05-12 00:07:14 +0300

    Add responsiveness to the components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,16 +8,15 @@ export default function Projects() {
             <h3>Some Things I've Built</h3>
             <div className="Projects-sect">
                 <img src={weatherapp} alt="Weather App" className="Projects-image"/>
-                <div className="Project-description-right">
+                <div className="Project-description Project-description-right">
                     <h4>Weather App</h4>
-                    <ul>
+                    <p>This project in the SheCodes Workshop tested my skills React, APIs, and AJAX. It also involved hosting the project and using Git and GitHub for version control, showcasing my ability to build dynamic web applications.</p>
+                    <ul className="Project-skills-right">
                         <li>AJAX</li>
                         <li>Boostrap</li>
                         <li>GitHub</li>
                         <li>React</li>
                     </ul>
-                    <p>This project in the SheCodes Workshop tested my skills React, APIs, and AJAX. It also involved hosting the project and using Git and GitHub for version control, showcasing my ability to build dynamic web applications.</p>
-                   
                     <div className="Links">
                         <div className="Project-link-right">
                             GitHub Repo
@@ -55,10 +54,10 @@ export default function Projects() {
 
             <div className="Projects-sect">
                 <img src={weatherapp} alt="Weather App" className="Projects-image"/>
-                <div className="Project-description-right">
+                <div className="Project-description Project-description-right">
                     <h4>Weather App</h4>
                     <p>This project in the SheCodes Workshop tested my skills React, APIs, and AJAX. It also involved hosting the project and using Git and GitHub for version control, showcasing my ability to build dynamic web applications.</p>
-                    <ul>
+                    <ul className="Project-skills-right">
                         <li>AJAX</li>
                         <li>Boostrap</li>
                         <li>GitHub</li>
